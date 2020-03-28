ee3d7de2249ba63b808c65036e85e95204f9b3c1
gleb-kov <kovg16@gmail.com>
2020-03-28 16:13:31 +0300

    passed. modus ponens, annotation

--- proof/proof_parser.cpp
+++ proof/proof_parser.cpp
@@ -3,12 +3,13 @@
 #include "proof_parser.h"
 
 TProofParser::TProofParser(std::string &statement, std::vector<std::string> &proof_body) {
-    head = parser.parse_context(statement);
-    parser.clear();
-    for (auto &it : proof_body) {
-        auto state = parser.parse(it);
-        proof.add(state);
-        parser.clear();
+    if (proof_body.empty()) {
+        throw parser_error("Proof is incorrect");
+    }
+    head = TParser().parse_context(statement);
+    for (size_t i = 0; i < proof_body.size(); i++) {
+        auto state = TParser().parse(proof_body[i]);
+        if (i + 1 < proof_body.size() && proof.contains(state)) continue;
         if (head->has_hypothesis(state)) {
             uint64_t num = head->get_hypothesis(state);
             proof_state.emplace_back(0, num);
@@ -19,30 +20,64 @@ TProofParser::TProofParser(std::string &statement, std::vector<std::string> &pro
             proof_dependency.emplace_back();
         } else {
             std::pair<size_t, size_t> mp = modus_ponens(state);
-            //if (mp.first == 0 || mp.second == 0) {
-            //    throw parser_error("Proof is incorrect");
-            //}
+            if (mp.first == 0 || mp.second == 0) {
+                throw parser_error("Proof is incorrect");
+            }
             proof_state.emplace_back(2, 0);
             proof_dependency.push_back({mp.first, mp.second});
         }
+        proof.add(state);
     }
-    assert(proof_body.size() == proof.size());
-    assert(proof_state.size() == proof.size());
-    assert(proof_dependency.size() == proof.size());
+    size_t last = proof.size();
+
+    assert(last == proof_state.size());
+    assert(last == proof_dependency.size());
+
+    if (proof[last] != head->get_statement()) {
+        throw parser_error("Proof is incorrect");
+    }
+
+    // minimize block
+    renumeration.assign(last + 1, 0);
+    std::queue<size_t> dep({last});
+    renumeration[last] = 1;
+    while (!dep.empty()) {
+        size_t cur = dep.front();
+        dep.pop();
+        for (size_t sub : proof_dependency[cur - 1]) {
+            if (renumeration[sub] == 0) {
+                renumeration[sub] = 1;
+                dep.push(sub);
+            }
+        }
+    }
+    size_t cnt = 0;
+    for (size_t i = 1; i < renumeration.size(); i++) {
+        cnt += renumeration[i];
+        if (renumeration[i]) renumeration[i] = cnt;
+    }
 }
 
-NGrammar::context TProofParser::get_statement() const {
+NGrammar::context TProofParser::get_context() const {
     return head;
 }
 
 std::pair<size_t, size_t> TProofParser::modus_ponens(NGrammar::expr const &e) {
+    for (auto it = proof.rev_impl_begin(e); it != proof.rev_impl_end(e); it++) {
+        size_t rhs_ind = *it;
+        auto rhs = proof[rhs_ind];
+        auto needed_lhs = NGrammar::to_binary(rhs)->get_lhs();
+        size_t lhs_ind = proof.get_index(needed_lhs);
+        if (lhs_ind) return {rhs_ind, lhs_ind};
+    }
     return {0, 0};
 }
 
 void TProofParser::print(std::ostream &s) {
     s << head->to_string() << std::endl;
     for (size_t i = 0; i < proof.size(); i++) {
-        s << '[' << i + 1 << ". ";
+        if (renumeration[i + 1] == 0) continue;
+        s << '[' << renumeration[i + 1] << ". ";
         switch (proof_state[i].first) {
             case 0: {
                 s << "Hypothesis " << proof_state[i].second;
@@ -53,23 +88,15 @@ void TProofParser::print(std::ostream &s) {
                 break;
             }
             case 2: {
-                s << "M.P. " << proof_dependency[i][0] << " " << proof_dependency[i][1];
+                s << "M.P. " << renumeration[proof_dependency[i][0]] << ", " << renumeration[proof_dependency[i][1]];
                 break;
             }
             default: {
-                throw parser_error("Undefined origin of statement.");
+                throw std::runtime_error("Undefined origin of statement.");
             }
         }
-        s << "] " << proof[i]->to_string() << std::endl;
+        s << "] " << proof[i + 1]->to_string() << std::endl;
     }
-}
-
-std::vector<NGrammar::expr>::iterator TProofParser::begin() {
-    return proof.begin();
-}
-
-std::vector<NGrammar::expr>::iterator TProofParser::end() {
-    return proof.end();
 }
 
 std::ostream &operator<<(std::ostream &s, TProofParser &solver) {
--- proof/proof_parser.h
+++ proof/proof_parser.h
@@ -5,6 +5,7 @@
 #include <ostream>
 #include <string>
 #include <vector>
+#include <queue>
 
 #include "../parser/parser.h"
 #include "../parser/parser_error.h"
@@ -16,25 +17,21 @@ private:
 
 private:
     context head;
-    TParser parser;
     TExprList proof;
     // hypothesis, axiom or modus ponens
     std::vector<std::pair<size_t, uint64_t>> proof_state;
     // connected expr's in proof
     std::vector<std::vector<size_t>> proof_dependency;
+    std::vector<size_t> renumeration;
 
 public:
     TProofParser(std::string &statement, std::vector<std::string> &proof_body);
 
-    context get_statement() const;
+    context get_context() const;
 
     std::pair<size_t, size_t> modus_ponens(expr const &);
 
-    void print(std::ostream &s);
-
-    [[deprecated]] std::vector<expr>::iterator begin();
-
-    [[deprecated]] std::vector<expr>::iterator end();
+    void print(std::ostream &);
 };
 
 std::ostream &operator<<(std::ostream &s, TProofParser &);
